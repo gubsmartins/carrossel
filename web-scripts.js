17e67edfbe0e66daef41e0bb7fd0e93908993eed
gubsmartins <76982769+gubsmartins@users.noreply.github.com>
2021-01-04 23:53:50 -0300

    Add files via upload

---
+++ web-scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    
+    $("ul.acoes-conta > li:first-child i").click(function(){
+        window.location.href="/conta/favorito/listar";
+    });
+    
+    $("ul.acoes-conta > li:nth-child(2) i").click(function(){
+        window.location.href="/conta/index";
+    });
+    
+    $("ul.acoes-conta > li:first-child i").attr('title', 'Lista de Desejos!');
+    $("ul.acoes-conta > li:nth-child(2) i").attr('title', 'Minha Conta');
+    
+});
