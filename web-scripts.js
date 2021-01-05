519f3a1e7c51e7828bf975643f7641d593cf5ace
gubsmartins <76982769+gubsmartins@users.noreply.github.com>
2021-01-04 23:51:42 -0300

    Update web-scripts.js

--- web-scripts.js
+++ web-scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         window.location.href="/conta/index";
     });
     
-    $("ul.acoes-conta > li:first-child i").attr('title', 'Lista de Desejos');
+    $("ul.acoes-conta > li:first-child i").attr('title', 'Lista de Desejos!');
     $("ul.acoes-conta > li:nth-child(2) i").attr('title', 'Minha Conta');
     
 });
